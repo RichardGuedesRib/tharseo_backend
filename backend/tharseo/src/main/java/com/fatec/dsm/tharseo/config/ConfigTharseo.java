38935aa6460bf1d65a95b706850210e4743b9bb2
Richard Guedes <richard.silva46@fatec.sp.gov.br>
2024-05-11 10:01:13 -0300

    fix(database) : adjust database with changes

--- backend/tharseo/src/main/java/com/fatec/dsm/tharseo/config/ConfigTharseo.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.dsm.tharseo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class ConfigTharseo {
-}
